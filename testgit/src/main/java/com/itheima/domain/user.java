688ad1ac6b7aea873805225ba047821ab77f30b2
xujianghua <857179914@qq.com>
2018-08-08 16:01:35 +0800

    第一次提交

---
+++ testgit/src/main/java/com/itheima/domain/user.java
@@ -0,0 +1,17 @@
+package com.itheima.domain;
+
+public class user {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
